93eca899f07b1ea8fb96c5d60474f4fc7f35fa5b
Jean Philippe Carlens <coruscant11@gmail.com>
2022-03-13 00:13:20 +0100

    Version fonctionelle

---
+++ commands/banner.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { token } = require('../config.json');
+const { getUserBanner } = require("discord-banner");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("banner")
+        .setDescription("Afficher la banner d'un utilisateur.")
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('Utilisateur de la banner.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('Banner principale ou de serveur.')
+                .setRequired(true)
+                .addChoice('Principale', 'Principale')),
+                
+    async execute(interaction) {
+        const client = interaction.client;
+
+        let user = interaction.options.getUser("user");
+        let type = interaction.options.getString("type");
+
+        client.users.fetch(user.id).then(fetchedUser => {
+            user = fetchedUser;
+        });
+        interaction.guild.members.fetch({ user, force: true });
+
+        let member = interaction.guild.members.cache.find(member => member.user == user)
+
+        let bannerURL;
+        console.log(`${interaction.user.tag} a fait la commande /banner pour voir la banner ${type} de ${user.tag}.`);
+        getUserBanner(user.id, {
+            token: token,
+            format: "gif"
+          }).then(banner => {
+            const bannerEmbed = {
+                title: user.username,
+                image: {
+                    url: banner.url
+                },
+                footer: {
+                    text: "Principale"
+                }
+            }
+            interaction.reply({ embeds: [bannerEmbed] });
+          });
+    }
+}
---
+++ commands/pp.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { mdLogin, mdPswd } = require('../config.json')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("pp")
+        .setDescription("Afficher la pp d'un utilisateur.")
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('Utilisateur de la pp.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('Pp principale ou de serveur.')
+                .setRequired(true)
+                .addChoice('Principale', 'Principale')
+                .addChoice('Serveur', 'Serveur')),
+    async execute(interaction) {
+        const client = interaction.client;
+
+        let user = interaction.options.getUser("user");
+        let type = interaction.options.getString("type");
+        interaction.guild.members.fetch({ user, force: true });
+        let member = interaction.guild.members.cache.find(member => member.user == user)
+
+        console.log(`${interaction.user.tag} a fait la commande /pp pour voir la pp ${type} de ${user.tag}.`);
+
+        const avatarFormat = { format: 'png', size: 1024 };
+        const ppEmbed = {
+            title: user.username,
+            image: {
+                url: type == "Principale" ? user.displayAvatarURL(avatarFormat) : member.displayAvatarURL(avatarFormat)
+            },
+            footer: {
+                text: type
+            }
+        }
+        interaction.reply({ embeds: [ppEmbed] });
+    }
+}
